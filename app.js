777cf19506d769993850335800e29fcf6c62df1b
multiplaie <multiplaie@protonmail.com>
2022-06-27 11:06:55 +0200

    add cash counter

--- app.js
+++ app.js
@@ -1,57 +1,59 @@
-const INCREMENT_SUCCESS_SCORE_BUTTON = 'a';
-const DECREMENT_SUCCESS_SCORE_BUTTON = 'w';
-const INCREMENT_FAIL_SCORE_BUTTON = 'z';
-const DECREMENT_FAIL_SCORE_BUTTON = 'x';
+const INCREMENT_2   = 'a';
+const INCREMENT_10  = 'z';
+const INCREMENT_25  = 'e';
+const INCREMENT_100 = 'r';
+const DECREMENT_2 = 'w';
+const DECREMENT_10 = 'x';
+const DECREMENT_25 = 'c';
+const DECREMENT_100 = 'v';
 
 
+var cash = 0;
 
-var hits = 0;
-var succedHits = 0;
-var failedHits = 0;
-var hitSuccessElement = document.querySelector(".success .hit");
-var hitFailElement = document.querySelector(".fail .hit");
-var hitTotalElement = document.querySelector(".total .hit");
+var cashElement = document.querySelector(".cash .hit");
+
 document.body.onkeyup = function (e) {
     switch(e.key){
-        case INCREMENT_SUCCESS_SCORE_BUTTON:
-            addSucessHit();
+        case INCREMENT_2:
+            addCash(2);
             break;
-        case DECREMENT_SUCCESS_SCORE_BUTTON:
-            removeSuccesHit();
+        case INCREMENT_10:
+            addCash(10);
             break;
-        case INCREMENT_FAIL_SCORE_BUTTON:
-            addFailHit();
+        case INCREMENT_25:
+            addCash(25);
             break;
-        case DECREMENT_FAIL_SCORE_BUTTON:
-            removeFailHit();
-            break;     
+        case INCREMENT_100:
+            addCash(100);
+            break;
+        case DECREMENT_100:
+            removeCash(100);
+            break;
+        case DECREMENT_25:
+            removeCash(25);
+            break;
+        case DECREMENT_10:
+            removeCash(10);
+            break;
+        case DECREMENT_2:
+            removeCash(2);
+            break;
     };
-    calculateTotal();
     renderHits();
 };
 
-var calculateTotal = function(){
-    hits = succedHits + failedHits; 
+
+var addCash = function (add) {
+    cash+= add;
 };
 
-var addSucessHit = function () {
-    succedHits++;
+var removeCash = function (remove) {
+    cash-= remove;
 };
 
-var removeSuccesHit = function () {
-    succedHits--;
-};
-var addFailHit = function () {
-    failedHits++;
-};
-var removeFailHit = function () {
-    failedHits--;
-};
 
 var renderHits = function () {
-    hitSuccessElement.innerHTML = succedHits;
-    hitFailElement.innerHTML = failedHits;
-    hitTotalElement.innerHTML = hits;
+    cashElement.innerHTML = cash;
 };
 
 renderHits();
