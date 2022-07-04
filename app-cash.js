eeb442bac28e8e7c9c72563f0eb5ff0fc228a423
Remy Jacquand <multiplaie@protonmail.com>
2022-07-04 12:09:44 +0200

    change command and value cash amount

--- app-cash.js
+++ app-cash.js
@@ -1,10 +1,10 @@
-const INCREMENT_2   = 'a';
+const INCREMENT_5   = 'a';
 const INCREMENT_10  = 'z';
-const INCREMENT_25  = 'e';
+const INCREMENT_20  = 'e';
 const INCREMENT_100 = 'r';
-const DECREMENT_2 = 'w';
+const DECREMENT_5 = 'w';
 const DECREMENT_10 = 'x';
-const DECREMENT_25 = 'c';
+const DECREMENT_20 = 'c';
 const DECREMENT_100 = 'v';
 
 
@@ -14,14 +14,14 @@ var cashElement = document.querySelector(".cash .hit");
 
 document.body.onkeyup = function (e) {
     switch(e.key){
-        case INCREMENT_2:
-            addCash(2);
+        case INCREMENT_5:
+            addCash(5);
             break;
         case INCREMENT_10:
             addCash(10);
             break;
-        case INCREMENT_25:
-            addCash(25);
+        case INCREMENT_20:
+            addCash(20);
             break;
         case INCREMENT_100:
             addCash(100);
@@ -29,14 +29,14 @@ document.body.onkeyup = function (e) {
         case DECREMENT_100:
             removeCash(100);
             break;
-        case DECREMENT_25:
-            removeCash(25);
+        case DECREMENT_20:
+            removeCash(20);
             break;
         case DECREMENT_10:
             removeCash(10);
             break;
-        case DECREMENT_2:
-            removeCash(2);
+        case DECREMENT_5:
+            removeCash(5);
             break;
     };
     renderHits();
